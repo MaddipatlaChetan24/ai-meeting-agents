d51c1c13095b749922e22216f4b0478acc8921ed
Maddipatla Chatan <chetan121318@gmail.com>
2026-08-23 21:50:13 +0530

    Update extractor.py

--- core/extractor.py
+++ core/extractor.py
@@ -8,17 +8,6 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 import os 
 import json
 
-def get_llm():
-    return ChatMistralAI(model = "mistral-small-latest", mistral_api_key = os.getenv("MISTRAL_API_KEY"),temperature=0.2)
-
-def build_chain(system_prompt : str):
-    llm = get_llm()
-    return (
-        RunnablePassthrough() | RunnableLambda(lambda x : {"text" : x}) |ChatPromptTemplate.from_messages([
-        ("system", system_prompt),
-        ("human","{text}"),
-    ]) | llm |StrOutputParser()
-    )
 
 def chunk_transcript_for_extraction(transcript: str) -> list:
     splitter = RecursiveCharacterTextSplitter(chunk_size=4000, chunk_overlap=200)
