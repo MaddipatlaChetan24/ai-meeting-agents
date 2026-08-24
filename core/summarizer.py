2f596da2c601068425f6685a4dd77152d5ad381f
Maddipatla Chatan <chetan121318@gmail.com>
2026-08-24 21:52:49 +0530

    Update summarizer.py

--- core/summarizer.py
+++ core/summarizer.py
@@ -6,21 +6,7 @@ from langchain_core.runnables import RunnablePassthrough, RunnableLambda
 
 import os 
 
-def get_llm():
-    return ChatMistralAI(model = "mistral-small-latest", mistral_api_key = os.getenv("MISTRAL_API_KEY"),temperature=0.3)
 
-
-    map_chain = map_prompt | llm | StrOutputParser()
-
-    chunks = split_transcript(transcript)
-
-   
-
-    combined_chain = (
-        RunnablePassthrough() | RunnableLambda(lambda x:{"text":x}) | combined_prompt | llm | StrOutputParser()
-    )
-
-    return combined_chain.invoke(combined)
 
 def generate_title(transcript : str) -> str:
     llm = get_llm()
